e0359c0da895d82f4d8200ba1252361761959ec4
wanrong <wanrong@wondersgroup.com>
2020-09-11 21:57:24 +0800

    [webpack+vue]测试async await

---
+++ src/components/common/Tab1.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="tab1">
+        <el-button type="primary" plain>Tab1</el-button>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            school : 'wonders'
+        };
+    },
+    methods: {
+        printSchool(){
+            console.log(this.school);
+        }
+    },
+    components: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
